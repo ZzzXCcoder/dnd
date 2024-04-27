3919ba2066ed074ebe28d421ab72061ff2ec6d95
ZzzXCcoder <makaka1337@list.ru>
2024-04-27 13:02:39 +0300

    start

--- Dndproject/main/views.py
+++ Dndproject/main/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     if request.method == 'POST':
-        input_text = request.POST.get('inputText', '') # Получаем значение поля ввода по его имени
+        input_text = request.POST.get('inputText', '')
         print(input_text)
         return HttpResponse(f'Введенный текст: {input_text}')
     else:
-        return HttpResponse('Метод не поддерживается')
+        return render(request, 'index.html')

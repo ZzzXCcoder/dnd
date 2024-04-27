7cab60ef44e736c963eb86a80210d50969bf70c1
ZzzXCcoder <makaka1337@list.ru>
2024-04-27 12:56:32 +0300

    Addd

--- Dndproject/main/views.py
+++ Dndproject/main/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, "index.html")
-# Create your views here.
+    if request.method == 'POST':
+        input_text = request.POST.get('inputText', '') # Получаем значение поля ввода по его имени
+        print(input_text)
+        return HttpResponse(f'Введенный текст: {input_text}')
+    else:
+        return HttpResponse('Метод не поддерживается')
